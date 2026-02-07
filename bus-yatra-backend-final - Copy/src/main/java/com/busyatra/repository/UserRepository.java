3106e165fa344e621b87e8f1c786ccec865a20db
Vardhan7396 <vardhanoggu481@gmail.com>
2026-02-07 14:07:38 +0530

    Removed duplicate backend folder

--- bus-yatra-backend-final - Copy/src/main/java/com/busyatra/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.busyatra.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.busyatra.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByEmail(String email);   // ✅ THIS LINE FIXES ERROR
-}
